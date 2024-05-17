4f76539262eb95374ea60a559ba2fd42b9ed9f38
ghnur31 <ghidayatnur@gmail.com>
2024-05-17 21:46:49 +0700

    create function for forgot password and send link reset password with mailtrap

--- resources/views/livewire/auth/login-page.blade.php
+++ resources/views/livewire/auth/login-page.blade.php
@@ -17,6 +17,13 @@
 
           <!-- Form -->
           <form wire:submit.prevent='save'>
+
+            @if(session('error'))
+              <div class="mt-2 bg-red-500 mb-4 text-sm text-white rounded-lg p-4" role="alert">
+                {{ session('error') }}
+              </div>
+            @endif
+
             <div class="grid gap-y-4">
               <!-- Form Group -->
               <div>
